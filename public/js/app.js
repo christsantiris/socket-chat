c91b2027d5ad948a302a6bb16669b018d8355ef7
Chris Tsantiris <chris.tsantiris@gmail.com>
2016-11-26 19:22:52 -0500

    chat app using socket.io works and deployed to heroku

---
+++ public/js/app.js
@@ -0,0 +1,45 @@
+var socket = io();
+var name = getQueryVariable("name") || "Anonymous";
+var room = getQueryVariable("room");
+
+console.log(name + "  wants to join " + room);
+jQuery(".room-title").text(room)
+
+socket.on("connect", function () {
+	console.log("connected to socket.io server!");
+	socket.emit("joinRoom", {
+		name: name,
+		room: room
+	})
+});
+
+socket.on("message", function (message) {
+	var momentTimeStamp = moment.utc(message.timestamp);
+	var $messages = jQuery(".messages");
+	var $message = jQuery('<li class="list-group-item"></li>');
+
+	console.log("New Message");
+	console.log(message.text);
+
+	$message.append("<p><strong>" + message.name + " " + momentTimeStamp.local().format("h:mm a") +  "</strong></p>");
+	$message.append("<p>" + message.text + "</p>");
+	$messages.append($message);
+});
+
+//Handles submitting of new messages
+
+var $form = jQuery("#message-form");
+
+$form.on("submit", function(event) {
+	event.preventDefault();
+
+	var $message = $form.find("input[name=message]");
+
+
+	socket.emit("message", {
+		name: name,
+		text: $message.val()
+	});
+
+	$message.val("");
+});
